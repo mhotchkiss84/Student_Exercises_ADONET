9669af00700995bedf421b18c87d8745fc628dcf
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-08-06 14:21:30 -0400

    Finished exercise 2

--- Student_Exercises_ADONET/Data/Repository.cs
+++ Student_Exercises_ADONET/Data/Repository.cs
@@ -71,6 +71,58 @@ namespace Student_Exercises_ADONET.Data
         }
         //A method to get all exercises by language type i.e. javascript
         //Will be like the above method except it needs a WHERE
+        public List<Exercise> GetExercisesByLanguage(string language)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                // Note, we must Open() the connection
+                conn.Open();
+
+                // We must "use" commands too.
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // Here we setup the command with the SQL we want to execute before we execute it.
+                    cmd.CommandText = "SELECT Id, Name FROM Exercise WHERE Language = @language";
+                    cmd.Parameters.Add(new SqlParameter("@language", language));
+
+                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // A list to hold the exercises we retrieve from the database.
+                    List<Exercise> Exercises = new List<Exercise>();
+
+                    // Read() will return true if there's more data to read
+                    while (reader.Read())
+                    {
+                        // The "ordinal" is the numeric position of the column in the query results.
+                        //  For our query, "Id" has an ordinal value of 0 and "DeptName" is 1.
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        // We user the reader's GetXXX methods to get the value for a particular ordinal.
+                        int idValue = reader.GetInt32(idColumnPosition);
+
+                        int NameColumnPosition = reader.GetOrdinal("Name");
+                        string NameValue = reader.GetString(NameColumnPosition);
+
+                        // Now let's create a new exercise object using the data from the database.
+                        Exercise Exercise = new Exercise
+                        {
+                            Id = idValue,
+                            Name = NameValue
+                        };
+
+                        // ...and add that exercise object to our list.
+                        Exercises.Add(Exercise);
+                    }
+
+                    // We should Close() the reader
+                    reader.Close();
+
+                    // Return the list of exercises
+                    return Exercises;
+                }
+            }
+        }
 
     }
 }
--- Student_Exercises_ADONET/Program.cs
+++ Student_Exercises_ADONET/Program.cs
@@ -20,11 +20,23 @@ namespace Student_Exercises_ADONET
             Console.WriteLine("Getting All Exercises:");
             Console.WriteLine();
             
-            //Creating a new list to store
+            //Creating a new list to store the exercises
             List<Exercise> allExercises = Repo.GetAllExercises();
 
-            //Using a loop to go through the list and print the exercise Id and the exercise name   
+            //Using a loop to go through the list and print the exercise Id and the exercise name
             foreach (Exercise exer in allExercises)
+            {
+                Console.WriteLine($"{exer.Id} {exer.Name}");
+            }
+
+            // Exercise 2: Find all the exercises in the database where the language is JavaScript.
+            Console.WriteLine("Getting exercises by language: javascript");
+            Console.WriteLine();
+
+            //Creating a list to store the exercises
+            List<Exercise> ExerciseByLanguage = Repo.GetExercisesByLanguage("Javascript");
+
+            foreach (Exercise exer in ExerciseByLanguage)
             {
                 Console.WriteLine($"{exer.Id} {exer.Name}");
             }
